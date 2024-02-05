9597d39d3dda553c0ace90aefd2b08614e14f03c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-02-05 10:48:22 +0100

    day1 of nodejs done

---
+++ week2/Day1/mathOperations.js
@@ -0,0 +1,27 @@
+// Function that Add two numbers and return the result.
+//    (x, y) => (x + y);
+
+function addNum(x, y) {
+    return x + y;
+}   
+// Function that Subtract two numbers and return the result.
+//    (x, y) => (x - y);
+   
+function SubtractNum(x, y) {
+    return x - y;
+} 
+// Function that Multiply two numbers and return the result.
+//    (x, y) => (x * y);
+      
+function MultiplyNum(x, y) {
+    return x * y;
+}       
+// Function that Divide two numbers and return the result.
+//     (x, y) => (x / y);
+function DivideNum(x, y) {
+    return x / y;
+}
+console.log(addNum(5, 9));
+console.log(SubtractNum(2, 4));
+console.log(MultiplyNum(2, 3));
+console.log(DivideNum(2, 0));

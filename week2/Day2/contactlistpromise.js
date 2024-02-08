4c553521b5b003054e26527ac5871b2354865fd5
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-02-08 12:22:49 +0100

    contact list with async/awiat

--- week2/Day2/contactlistpromise.js
+++ week2/Day2/contactlistpromise.js
@@ -7,10 +7,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const prompt = (question)=>{
+    return new Promise((resolve, reject) => {
+        rl.question(question, (UserInput)=>{
+            resolve(UserInput);
+        })
+    })
+}
 
-
-const contactMenu = () => {
-    rl.question(`|----------------------------------|\nChoose One of the Option Below\n1-Add a Contact\n2-View all contacts\n3-Search for a contact\n4-Exit\n`,(val)=>{
+const contactMenu = async() => {
+    let val = await prompt(`|----------------------------------|\nChoose One of the Option Below\n1-Add a Contact\n2-View all contacts\n3-Search for a contact\n4-Exit\n`);
         switch (val) {
             case '1':
                { addContact();
@@ -30,38 +36,33 @@ const contactMenu = () => {
                 console.log(`choose right number`);
                 contactMenu()
             }
-        }
-    })
-}
+                }
+    }
 
-const addContact = () => {
-    rl.question(`Enter Contact Name : `,(name)=>{
-        rl.question(`Enter Contact Number : `,(number)=>{
-            contacts.push({name:name,number:number})
-            console.log(`Contact Added Successfully`);
-            contactMenu();
-        })
-    })
+const addContact = async () => {
+    let name = await prompt(`Enter Contact Name : `);
+    let number = await prompt(`Enter Contact Number : `);
+        contacts.push({name:name,number:number})
+        console.log(`Contact Added Successfully`);
+        contactMenu();
 };
 const viewContact = () => {
     contacts.forEach((contact) => console.log(`Name: ${contact.name}\nPhone Number: ${contact.number}`))
     contactMenu();
 };
-const searchContact = () => {
-    rl.question(`Enter Searched Contact name:\n`,(name) => {
+const searchContact = async() => {
+    let name = await prompt(`Enter Searched Contact name:\n`);
         let result = contacts.find((contact) => contact.name === name);
         if (result) {
             console.log(`Contact Found\nName: ${result.name}\nPhone Number: ${result.number}`);
         }
         else
-            console.log(`Contact Not Found`);
-        rl.question(`Tap 0 to go back to menu other to exit\n`,(val)=>{
+        console.log(`Contact Not Found`);
+        let val = await prompt(`Tap 0 to go back to menu other to exit\n`);
             if (val === '0') {
                 contactMenu();
             }else
                 rl.close();
-        })
-    })
 };
 
 contactMenu()
